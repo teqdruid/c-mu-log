e4d1bbffbcab69c4d479c8a8e77e9d902560b7e9
teqdruid <teqdruid@d6f54c02-84fe-11dd-90a9-6306d3ffb32f>
2008-11-24 21:39:38 +0000

    Oops

---
+++ src/culog.ml
@@ -0,0 +1,22 @@
+(*
+*  culog.ml
+*
+*  Made by (John Demme)
+*  Login   <teqdruid@teqBook>
+*
+*  Started on  Mon Nov 24 16:03:20 2008 John Demme
+*  Last update Mon Nov 24 16:03:27 2008 John Demme
+*)
+
+open Interp
+
+let _ = 
+  let lexbuf = Lexing.from_channel (open_in Sys.argv.(1)) in
+  let program = Parser.program Scanner.token lexbuf in
+  let pDB = parseDB(program) in
+    (*print_string "Database dump:\n";
+    dump_db pDB;
+    print_string "\n";*)
+    let sGen = query pDB {name = "main"; params = []} in
+      iter_sols sGen
+;;
